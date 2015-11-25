df88785f044fe9c786745565476bbde585d9a665
djturner1984@gmail.com <atitagain>
2015-11-25 21:53:57 +1100

    first commit

---
+++ Project.Name/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace Project.Name.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new VirtualFileResult("~/app/index.html", "text/html");
+        }
+    }
+}
---
+++ RenameProject.ps1
@@ -0,0 +1,23 @@
+$name = Read-Host 'What is your new project namespace?'
+$nameToReplace = "Domain.Reports"
+$wildcardName = "*"+$nameToReplace + "*"
+If ($name -Match " ")
+{
+    "No spaces allowed"
+    exit
+}
+
+Get-ChildItem -filter $wildcardName -Recurse | Rename-Item -NewName { $_.Name -replace $nameToReplace, $name } | where-object { $_.Name -notMatch "packages"  }
+$files=get-childitem . *.* -rec | where { ! $_.PSIsContainer }
+foreach ($file in $files)
+{
+    If ($file.PSPath -Match ".ps1" -Or $file.PSPath -Match "packages" -Or $file.PSPath -Match ".dll")
+    {
+        Write-Host $file.PSPath ' is in the excluded list so ignoring it'
+    }
+    Else
+    {
+        Write-Host 'Processing ' + $file.PSPath
+        (Get-Content $file.PSPath) | Foreach-Object {$_ -replace $nameToReplace, $name} | Set-Content $file.PSPath
+    }
+}
